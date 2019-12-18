add9ffc9eba280b519b764f74f5c7fa7b2e63b00
Will Parks <wparks08@gmail.com>
2019-12-17 21:05:14 -0800

    add survey submission

---
+++ app/data/questions.js
@@ -0,0 +1,14 @@
+module.exports = {
+    questions: [
+        "I see myself as extroverted and enthusiastic",
+        "I see myself as critical and quarrelsome",
+        "I see myself as dependable and self disciplined",
+        "I see myself as anxious and easily upset",
+        "I see myself as open to new experiences and complex",
+        "I see myself as reserved and quiet",
+        "I see myself as sympathetic and warm",
+        "I see myself as disorganized and careless",
+        "I see myself as calm and emotionally stable",
+        "I see myself as conventional and uncreative"
+    ]
+};
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,18 +2,45 @@ var express = require("express");
 var router = express.Router();
 
 var friends = require("../data/friends");
+var questions = require("../data/questions");
 
 router.get("/friends", (req, res) => {
     res.json(friends);
 });
 
+router.get("/questions", (req, res) => {
+    res.json(questions);
+})
+
 router.post("/friends", (req, res) => {
-    friends.push({
+    let newFriend = {
         name: req.body.name,
         photo: req.body.photo,
         scores: req.body.scores
-    });
-    res.json(friends);
+    }
+
+    let match = findMatch(newFriend, friends);
+    friends.push(newFriend);
+
+    res.json(match);
 });
+
+function findMatch(newFriend, friends) {
+    let closestScore;
+    let match;
+    //Below not working yet...
+    friends.forEach(friend => {
+        let totalScore = newFriend.scores.map((value, index, array) => { Math.abs(parseInt(value) - parseInt(friend.scores[index])) }).reduce((a, b) => a + b);
+        
+
+        if ((closestScore == null) || (totalScore < closestScore)) {
+            closestScore = totalScore;
+            match = friend;
+        }
+        
+    });
+    
+    return match;
+}
 
 module.exports = router;
