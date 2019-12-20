2645434ee5eba727512eeafe673e144fdcba73d0
Will Parks <wparks08@gmail.com>
2019-12-19 23:17:00 -0800

    update with real data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 module.exports = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5,1,2,3,4,5,1,2,4,5]
+        "name": "Will Parks",
+        "photo": "https://scontent.fsac1-2.fna.fbcdn.net/v/t1.0-9/p720x720/43456478_10213894372647082_5387539407676899328_o.jpg?_nc_cat=110&_nc_ohc=oxgz5Dr4QE8AQnkc60UECmJb-_GFIA4yJ5oV0aHEGgvWu0oyzPdOuOQXg&_nc_ht=scontent.fsac1-2.fna&oh=b95b0cbb1063d4fec60e86fa36292c8e&oe=5EA88582",
+        "scores":[2,1,5,4,5,5,5,3,3,1]
     }
 ]
