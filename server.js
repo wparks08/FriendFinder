d21c8f93a88270b7c59b837b50a01b8c7111df8a
Will Parks <wparks08@gmail.com>
2019-12-19 22:30:06 -0800

    refactor front-end js into its own file

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("app/public"));
 
 app.use("/api", api);
 app.use("/", html);
